e3706ace598c70c813ed780f923d1405913de68d
Samarth Keskar <samarthkeskar363@gmail.com>
2019-03-26 10:21:55 +0530

    writing a file using node fs module

--- CodingChallenge/Week 6/write a file using node fs module/app.js
+++ CodingChallenge/Week 6/write a file using node fs module/app.js
@@ -1,9 +1,14 @@
 "use strict";
 let fs = require("fs");
 let messageWriter = (fileName, msg, n) => {
+  let data = "";
   for (let i = 0; i < n; i++) {
-    fs.appendFileSync(fileName, msg);
+    data = data + msg;
   }
+  fs.writeFile(fileName, data, err => {
+    if (err) throw err;
+    console.log("File has been written successfully");
+  });
 };
 
 messageWriter("hello.txt", "Welcome to AttainU\n", 50);
