fde648635644cffc4f4a267354edb81ba2fe032b
Samarth Keskar <samarthkeskar363@gmail.com>
2019-04-08 10:18:06 +0530

    finding books for the specific language from 100 books in the mongoDB database using express

---
+++ CodingChallenge/Week 8/books library app/app.js
@@ -0,0 +1,59 @@
+"use strict";
+
+const express = require("express");
+
+const bodyParser = require("body-parser");
+
+const mongo = require("mongodb");
+
+const app = express();
+
+app.set("view engine", "hbs");
+
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use(bodyParser.json());
+
+app.use(express.static("public"));
+
+let DB;
+
+// Create a Mongo client
+let mongoClient = new mongo.MongoClient("mongodb://localhost:27017/library", { useNewUrlParser: true });
+mongoClient.connect(function(error) {
+    (error) ? console.log("Error connecting to the database."): DB = mongoClient.db("library");
+});
+
+
+app.get('/', (req, res) => {
+    res.render("index.hbs");
+});
+
+app.post('/', (req, res) => {
+
+    let query = {
+        language: req.body.language
+    }
+    DB.collection("books").find(query).toArray(function(error, books) {
+
+        if (error) {
+            console.log("error occured while connecting to collection");
+        } else {
+            let searchBooks = {
+                books: books
+            };
+
+            console.log(searchBooks.books);
+            res.render("search.hbs", searchBooks);
+        }
+
+    });
+});
+
+
+
+
+
+
+
+app.listen(8080);
