5a378a597847b320829556086c7d95c8a1d9a633
Samarth Keskar <samarthkeskar363@gmail.com>
2019-05-20 10:21:12 +0530

    generating prime numbers

---
+++ CodingChallenge/Week 14/generating prime numbers/app.js
@@ -0,0 +1,13 @@
+let primeNum = num => {
+    let counter = 0;
+    for(let i=1;i<=num;i++){ if(num % i == 0) counter = counter +1;}
+   return  counter > 2 ? false : true;
+  }
+  
+  let getPrimeNumbers = num => {
+    let primeArray = [];
+    for(var i=2;primeArray.length < num;i++){ if(primeNum(i)) primeArray.push(i);}
+    console.log(primeArray);
+  }
+
+  getPrimeNumbers(10);
