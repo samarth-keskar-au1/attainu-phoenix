9387680f47155f5ceb5475527419a53539e246f8
Samarth Keskar <samarthkeskar363@gmail.com>
2019-08-08 09:55:18 +0530

    Javascript function that calculates the maximum subarray sum.

---
+++ CodingChallenge/Week 24/Javascript function that calculates the maximum subarray sum./app.js
@@ -0,0 +1,17 @@
+"use strict";
+
+const getMaxSubSum = arr => {
+  let maxSum = 0;
+  let tempSum = 0;
+
+  for (let num of arr) { 
+    tempSum += num;
+    maxSum = Math.max(maxSum, tempSum);
+    if (tempSum < 0) tempSum = 0;
+  }
+
+  return maxSum;
+
+}
+
+console.log(getMaxSubSum([-1, 2, 4,-3, 5, 2,-5, 2]));
