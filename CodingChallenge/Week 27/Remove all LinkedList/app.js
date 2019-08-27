d50fddb56148c77574fd4e561e3ffb89d6ca9524
Samarth Keskar <samarthkeskar363@gmail.com>
2019-08-27 10:55:57 +0530

    removing all occurences of a value

---
+++ CodingChallenge/Week 27/Remove all LinkedList/app.js
@@ -0,0 +1,41 @@
+"use strict";
+const LinkedList = require("./list");
+
+class RemoveAll extends LinkedList {
+  constructor(value) {
+    super(value);
+    this.value = value;
+  }
+
+  removeAll(value) {
+		if (this.head.value === value) {
+       this.head = this.head.next;
+       this.removeAll(value);
+		} else {
+			let previousNode = this.head;
+			let currentNode = previousNode.next;
+
+			while(currentNode) {
+				if (currentNode.value === value) {
+					previousNode.next = currentNode.next;
+          currentNode = currentNode.next;
+				} else {
+					previousNode = currentNode;
+					currentNode = currentNode.next;
+				}
+			}
+		}	
+	}
+}
+
+
+let rm = new RemoveAll();
+
+
+rm.append(10);
+rm.append(20);
+rm.append(10);
+rm.append(20);
+rm.print();
+rm.removeAll(10);
+rm.print();
